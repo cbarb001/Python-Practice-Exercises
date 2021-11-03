f7d2a9d7249c51e6187872fe07df870757412a54
cbarb001 <cbarb001@gmail.com>
2021-11-02 20:34:03 -0700

    Complete extra 2

--- Odd_Even_Challenge.py
+++ Odd_Even_Challenge.py
@@ -9,9 +9,23 @@
 # one number to divide by (check). If check divides evenly into num,
 # tell that to the user. If not, print a different appropriate message.
 
-guess = int(input("Enter any number greater than 0: "))
+# MAIN PROGRAM
+num = int(input("Enter any number greater than 0 to check: "))
 
-if guess % 2 == 0:
+
+if num % 2 == 0:
     print("You chose an even number.")
 else:
     print("You chose an odd number.")
+
+# EXTRA 1
+if num % 4 == 0:
+    print("Your number is also divisible by 4.")
+
+# EXTRA 2
+check = int(input("Enter a new number to see if your first number is divisible by this new number. "))
+
+if num % check == 0:
+    print("{0} is divisible by {1} ".format(num, check))
+else:
+    print("{0} is NOT divisible by {1} ".format(num, check))
